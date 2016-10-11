8ad3a9dfcc18faea78d85f88e9a9cc86aac1749f
Laura Santana <laurystana@gmail.com>
2016-10-10 23:52:45 -0300

    Adding new record

--- app/controllers/todos/new.js
+++ app/controllers/todos/new.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  actions:{
+
+    addTodo: function(){
+      var date = this.get('date');
+      var title = this.get('title');
+      var body = this.get('body');
+
+      //Create todo
+      var newTodo = this.store.createRecord('todo', {
+        title: title,
+        body: body,
+        date: new Date(date)
+      });
+
+      //Save to firebase
+      newTodo.save();
+
+      //Clear frm
+      this.setProperties({
+        title: '',
+        body: '',
+        date: ''
+      });
+    }
+
+  }
+
 });
