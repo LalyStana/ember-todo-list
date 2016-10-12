f489ab678256034d730fac25bcdd4f5988308074
Laura Santana <laurystana@gmail.com>
2016-10-11 23:33:16 -0300

    Add editing functionality

--- app/controllers/todos/edit.js
+++ app/controllers/todos/edit.js
@@ -1,4 +1,45 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  actions: {
+
+    editTodo: function(id){
+
+      var date = this.get('model.date');
+      var title = this.get('model.title');
+      var body = this.get('model.body');
+
+      this.store.findRecord('todo', id).then((todo)=>{
+
+        todo.set('title', title);
+        todo.set('body', body);
+        todo.set('date', new Date(date));
+
+        //Save to firebase
+        todo.save();
+
+        this.transitionToRoute('todos');
+
+      });
+
+    },
+
+    deleteTodo: function(id){
+
+      this.store.findRecord('todo', id).then((todo)=>{
+
+        todo.deleteRecord();
+
+        //Save to firebase
+        todo.save();
+
+        this.transitionToRoute('todos');
+
+      });
+
+    }
+
+  }
+
 });
